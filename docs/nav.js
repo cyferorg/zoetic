14dfde92b93b317bea0d3e338f1c31be7f11baf9
bob <cyferltd@gmail.com>
2018-07-29 11:15:25 +0100

    fix js?

--- docs/nav.js
+++ docs/nav.js
@@ -1,4 +1,4 @@
-document.write('\">
+document.write('\
 \<div id="mySidenav" class="sidenav">\
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>\
   <a href="../index.html">Introduction</a>\
