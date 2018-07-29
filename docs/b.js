cf36dc1003c7d5bd2f4e0c493204b7843a44c01a
bob <cyferltd@gmail.com>
2018-07-29 11:26:33 +0100

    still fixing

--- docs/b.js
+++ docs/b.js
@@ -1,9 +1,9 @@
 document.write('\
 \
-    <h1>Add your HTML code here</h1>\
+<h1>Add your HTML code here</h1>\
 \
-     <p>Notice however, that you have to escape LF's with a '\', just like\
-        demonstrated in this code listing.\
-    </p>\
+<p>Notice however, that you have to escape LF's with a '\', just like\
+demonstrated in this code listing.\
+</p>\
 \
 ');
