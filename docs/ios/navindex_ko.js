716a6e238d8a7d77ab04e125ecdd2d072da09cde
bob <cyferltd@gmail.com>
2019-04-22 09:39:36 +0100

    add korean screenshots

--- docs/ios/navindex_ko.js
+++ docs/ios/navindex_ko.js
@@ -5,10 +5,10 @@ document.write('\
         <li><a href="#examples">비디오 예제</a></li>\
         <li><a href="#instructions">Zoetic 사용법</a></li>\
         <li><a href="firstproject/firstproject_ko.html">첫번째 프로젝트</a></li>\
-		<li><a href="menucommands/menucommands_ko.html">Menu commands</a></li>\
+		<li><a href="menucommands/menucommands_ko.html">메뉴 명령</a></li>\
 		<li><a href="furtherdiscussion/furtherdiscussion_ko.html">추후 논의</a></li>\
-		<li><a href="crop/crop_ko.html">Crop</a></li>\
-		<li><a href="layers/layers_ko.html">Fun with layers</a></li>\
+		<li><a href="crop/crop_ko.html">자르기</a></li>\
+		<li><a href="layers/layers_ko.html">레이어가있는 재미</a></li>\
 		<li><a href="feedbackprivacy/feedbackprivacy_ko.html">피드백 및 버그 알림</a></li>\
       </ul>\
     </div>\
